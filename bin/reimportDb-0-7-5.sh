974ce70d1dc9030620a01e61efba118d5628a0dc
Petr Kadlec <mormegil@centrum.cz>
2020-01-03 16:46:50 +0100

    Fix 0.7.5 import

--- bin/reimportDb-0-7-5.sh
+++ bin/reimportDb-0-7-5.sh
@@ -3,13 +3,13 @@
 set -e
 
 mysql at075 < at-0-7-5-structure.sql
-bin/console database:importCondition --version=0.7.5
-bin/console database:importItem --version=0.7.5
-bin/console database:importQuest --version=0.7.5
-bin/console database:importDroplist --version=0.7.5
-bin/console database:importMonster --version=0.7.5
-bin/console database:importMap --version=0.7.5
-bin/console database:importDialog --version=0.7.5
-bin/console database:assignItemQuest --version=0.7.5
-bin/console database:assignNpcQuest --version=0.7.5
-bin/console map:createAll --version=0.7.5
+bin/console database:importCondition 0.7.5
+bin/console database:importItem 0.7.5
+bin/console database:importQuest 0.7.5
+bin/console database:importDroplist 0.7.5
+bin/console database:importMonster 0.7.5
+bin/console database:importMap 0.7.5
+bin/console database:importDialog 0.7.5
+bin/console database:assignItemQuest 0.7.5
+bin/console database:assignNpcQuest 0.7.5
+bin/console map:createAll 0.7.5
